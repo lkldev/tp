2fa631362d7844d22aa3167481c076c6df975158
lkldev <lkldev@outlook.com>
2022-04-01 05:24:07 +0800

    Find assessment

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -2,6 +2,9 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import seedu.address.commons.core.Messages;
 import seedu.address.model.Model;
 import seedu.address.model.entity.EntityType;
@@ -35,8 +38,30 @@ public class FindCommand extends Command {
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.updateFilteredStudentList(predicate);
-        return new CommandResult(String.format(Messages.MESSAGE_STUDENTS_LISTED_OVERVIEW,
-                        model.getFilteredStudentList().size()), EntityType.STUDENT);
+        String result = String.format(Messages.MESSAGE_STUDENTS_LISTED_OVERVIEW,
+                model.getFilteredStudentList().size());
+        List<String> assessments = model.getFilteredStudentList().stream().map(student ->
+                model.getUnfilteredAssessmentList().stream()
+                        .filter(assessment -> assessment.getModule().hasStudent(student))
+                        .map(assessment ->
+                    assessment.getAttemptOfStudent(student).map(grade ->
+                            String.format("Student %s (%s)| module %s: %s: %d\n",
+                                    student.getName(), student.getStudentId(), assessment.getModule().getModuleCode(),
+                                    assessment.getAssessmentName(), grade.value))
+                            .orElse(String.format("Student %s (%s)| has no attempt for %s: %s\n",
+                                    student.getName(), student.getStudentId(), assessment.getModule().getModuleCode(),
+                                    assessment.getAssessmentName())))
+                        .reduce("", (x, y) -> x + y))
+                .collect(Collectors.toList());
+        result += "\n";
+        for (String s : assessments) {
+            String[] output = s.split("\\|", 2);
+            String name = output[0];
+            String nameRegex = name.replace("(", "\\(").replace(")", "\\)");
+            String attempts = output[1].replaceAll(nameRegex, "\t");
+            result += name + "\n\t|" + attempts + "\n";
+        }
+        return new CommandResult(result, EntityType.STUDENT);
     }
 
     @Override
